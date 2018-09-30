37fa20816aa69d86e0d9bc40ea99bb3ed4186579
福居 <fuju.cjd@alibaba-inc.com>
2018-09-30 13:07:38 +0800

    plugins

--- install_all_plugins.sh
+++ install_all_plugins.sh
@@ -1,23 +1,10 @@
 
-  gem install jekyll-paginate
-  gem install  jekyll-gist
-  gem install  jekyll-language-plugin
-  gem install  feed
-  gem install  scholar
-  gem install  tagging
-  gem install  mentions
-  gem install  sitemap generator
-  gem install  redirect
-  gem install  analytics
-  gem install  git-hub metadata
+ gem install  jekyll-feed
+ gem install  jekyll-redirect-from
+ gem install  jekyll-seo-tag
+ gem install  jekyll-sitemap
+ gem install  jekyll-avatar
+ gem install  jemoji
+ gem install  jekyll-mentions
+ gem install  jekyll-include-cache
 
-  ERROR:  Could not find a valid gem 'Redirect' (>= 0) in any repository
-ERROR:  Possible alternatives: redirect
-ERROR:  Could not find a valid gem 'From' (>= 0) in any repository
-ERROR:  Possible alternatives: from
-ERROR:  Could not find a valid gem 'Analytics' (>= 0) in any repository
-ERROR:  Possible alternatives: analytics
-ERROR:  Could not find a valid gem 'GitHub' (>= 0) in any repository
-ERROR:  Possible alternatives: git-hub
-ERROR:  Could not find a valid gem 'Metadata' (>= 0) in any repository
-ERROR:  Possible alternatives: metadata
